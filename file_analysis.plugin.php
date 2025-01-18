e023f2cc1d28ac9a9a48d55d41b2a9af5eb31cc0
Ade Ismail Siregar <adeismailbox@gmail.com>
2025-01-18 18:43:58 +0700

    Update file_analysis.plugin.php

--- file_analysis.plugin.php
+++ file_analysis.plugin.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * Plugin Name: File Access Analysis
- * Plugin URI: https://example.com/plugin
+ * Plugin URI: -
  * Description: Menampilkan analisis akses file dengan grafik dan tabel di halaman admin SLiMS.
- * Version: 1.0.0
- * Author: Pengembang Plugin
+ * Version: 0.1
+ * Author: Ade Ismail Siregar
+ * inspired by SQL from Hendro Wicaksono in SLiMS community WhatsApp 
  */
 
 use SLiMS\Plugins;
